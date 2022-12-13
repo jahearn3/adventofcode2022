bce913d9ba83bc23a5b2e4218d730c403b5e93e7
jahearn3 <josephahearn3@gmail.com>
2022-12-13 11:41:23 -0800

    day 9 double stars

--- day09.py
+++ day09.py
@@ -76,3 +76,46 @@ data = ld.load_data('input09.txt')
 print(f'Part 1: {simulate(data)}') # 2985 was too low, 5619 was the answer
 
 # Part 2
+
+def move_tail2(head_pos, tail_pos):
+    delta_x = head_pos[0] - tail_pos[0]
+    delta_y = head_pos[1] - tail_pos[1]
+    if((abs(delta_x)) + abs(delta_y) == 2): # diagonal motion not required
+        if((delta_x == 2) and (delta_y == 0)):
+            tail_pos[0] += 1
+        elif((delta_x == -2) and (delta_y == 0)):
+            tail_pos[0] -= 1
+        elif((delta_y == 2) and (delta_x == 0)):
+            tail_pos[1] += 1
+        elif((delta_y == -2) and (delta_x == 0)):
+            tail_pos[1] -= 1
+    # circumstances that require diagonal motion
+    elif((abs(delta_x)) + abs(delta_y) > 2):
+        if(delta_x > 0):
+            tail_pos[0] += 1
+        else:
+            tail_pos[0] -= 1
+        if(delta_y > 0):
+            tail_pos[1] += 1
+        else:
+            tail_pos[1] -= 1
+    return tail_pos
+
+def simulate2(data):
+    init_x = 0; init_y = 0
+    knots = []
+    for i in range(10): # initializing
+        knots.append([init_x, init_y])
+    tail_pos_visited = []
+    tail_pos_visited.append(str(knots[-1]))
+    for line in data:
+        direction, magnitude = line.split()
+        for i in range(int(magnitude)):
+            knots[0] = move_head(knots[0], direction)
+            for j in range(1, len(knots)):
+                knots[j] = move_tail2(knots[j-1], knots[j])
+            tail_pos_visited.append(str(knots[-1]))
+
+    return len(set(tail_pos_visited)) 
+
+print(f'Part 2: {simulate2(data)}') # 2376 was the answer
