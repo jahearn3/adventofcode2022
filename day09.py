ce6730769fc00f3919991cfe5acb64c18cd76b03
jahearn3 <josephahearn3@gmail.com>
2022-12-12 17:18:08 -0800

    day 9 first star

--- day09.py
+++ day09.py
@@ -16,17 +16,6 @@ def move_head(head_pos, direction):
 def move_tail(head_pos, tail_pos):
     delta_x = head_pos[0] - tail_pos[0]
     delta_y = head_pos[1] - tail_pos[1]
-    # dist = (delta_x**2 + delta_y**2)**0.5
-    # if(dist > 1.7): # diagonal motion required
-    #     if(delta_x > 0):
-    #         tail_pos[0] += 1
-    #     else:
-    #         tail_pos[0] -= 1
-    #     if(delta_y > 0):
-    #         tail_pos[1] += 1
-    #     else:
-    #         tail_pos[1] -= 1
-    # else:
     if((delta_x == 2) and (delta_y == 0)):
         tail_pos[0] += 1
     elif((delta_x == -2) and (delta_y == 0)):
@@ -36,28 +25,28 @@ def move_tail(head_pos, tail_pos):
     elif((delta_y == -2) and (delta_x == 0)):
         tail_pos[1] -= 1
     # circumstances that require diagonal motion
-    elif((delta_x == 2) and (delta_y <= 1)):
+    elif((delta_x == 2) and (delta_y == 1)):
         tail_pos[0] += 1
         tail_pos[1] += 1
-    elif((delta_x == 2) and (delta_y >= -1)):
+    elif((delta_x == 2) and (delta_y == -1)):
         tail_pos[0] += 1
         tail_pos[1] -= 1
-    elif((delta_x == -2) and (delta_y <= 1)):
+    elif((delta_x == -2) and (delta_y == 1)):
         tail_pos[0] -= 1
         tail_pos[1] += 1
-    elif((delta_x == -2) and (delta_y >= -1)):
+    elif((delta_x == -2) and (delta_y == -1)):
         tail_pos[0] -= 1
         tail_pos[1] -= 1
     elif((delta_x == 1) and (delta_y == 2)):
         tail_pos[0] += 1
         tail_pos[1] += 1
-    elif((delta_x >= -1) and (delta_y == 2)):
+    elif((delta_x == -1) and (delta_y == 2)):
         tail_pos[0] -= 1
         tail_pos[1] += 1
-    elif((delta_x <= 1) and (delta_y == -2)):
+    elif((delta_x == 1) and (delta_y == -2)):
         tail_pos[0] += 1
         tail_pos[1] -= 1
-    elif((delta_x <= -1) and (delta_y == -2)):
+    elif((delta_x == -1) and (delta_y == -2)):
         tail_pos[0] -= 1
         tail_pos[1] -= 1
     return tail_pos
@@ -70,20 +59,20 @@ def simulate(data):
     tail_pos_visited.append(str(tail_pos))
 
     for line in data:
-        direction = line[0]
-        magnitude = line[2]
+        direction, magnitude = line.split()
+        # direction = line[0]
+        # magnitude = line[2] # this line was the source of my errors: it only accounted for magnitudes up to 9
         for i in range(int(magnitude)):
             head_pos = move_head(head_pos, direction)
             tail_pos = move_tail(head_pos, tail_pos)
-            if(len(tail_pos_visited) < 50):
-                print(head_pos, tail_pos)
             tail_pos_visited.append(str(tail_pos))
 
-    # print(tail_pos_visited)
     return len(set(tail_pos_visited))
 
 data = ld.load_data('day09example1.txt')
 print(f'Example: {simulate(data)}')
 
 data = ld.load_data('input09.txt')
-print(f'Part 1: {simulate(data)}') # 2985 was too low
+print(f'Part 1: {simulate(data)}') # 2985 was too low, 5619 was the answer
+
+# Part 2
