fb05c070c2d171594f3144ba400f2609c91e6757
jahearn3 <josephahearn3@gmail.com>
2023-01-06 13:16:17 -0800

    day 14 function to draw rock paths

--- day14.py
+++ day14.py
@@ -1,6 +1,7 @@
 # Regolith Reservoir
 
 import load_data as ld 
+import numpy as np 
 
 def find_endpoints(data):
     endpoints = []
@@ -24,15 +25,48 @@ def find_endpoints(data):
         endpoints.append(coordinates)
     return endpoints, min_x, max_x, min_y, max_y
 
-# def draw_rock_paths(endpoints, min_x, max_x, min_y, max_y):
-#     grid = []
-#     for i in range(min_x, max_x + 1):
-#         for j in range(min_y, max_y + 1):
-#             grid.append()
+def draw_rock_paths(endpoints, min_x, max_x, min_y, max_y):
+    grid = np.zeros((max_x - min_x + 1, max_y - min_y + 1))
+    # for i in range(min_x, max_x + 1):
+    #     for j in range(min_y, max_y + 1):
+    #         grid.append()
+    for e in endpoints:
+        
+        # print(e)
+        # print(e[0][0], e[0][1])
+        # grid[e[0][0] - min_x][e[0][1] - min_y] = 1
+        
+        for c in range(len(e) - 1): 
+            
+            # print(e[c][0], e[c][1])
 
+            delta_x = e[c+1][0] - e[c][0]
+            delta_y = e[c+1][1] - e[c][1]
 
-#     return grid
+            if(delta_x > 0):
+                for i in range(delta_x):
+                    # print(e[c][0] + i, e[c][1])
+                    grid[e[c][0] + i - min_x][e[c][1]] = 1
+            elif(delta_x < 0):
+                for i in range(-delta_x):
+                    # print(e[c][0] - i, e[c][1])
+                    grid[e[c][0] - i - min_x][e[c][1]] = 1
+            if(delta_y > 0):
+                for i in range(delta_y):
+                    # print(e[c][0], e[c][1] + i)
+                    grid[e[c][0] - min_x][e[c][1] + i] = 1
+            elif(delta_y < 0):
+                for i in range(-delta_y):
+                    # print(e[c][0], e[c][1] - i)
+                    grid[e[c][0] - min_x][e[c][1] - i] = 1
+
+        # print(e[-1][0], e[-1][1])
+        grid[e[-1][0] - min_x][e[-1][1]] = 1
+
+    return grid
 
 data = ld.load_data('day14example1.txt')
 endpoints, min_x, max_x, min_y, max_y = find_endpoints(data)
-# grid = draw_rock_paths(endpoints, min_x, max_x, min_y, max_y)
+# print(endpoints, min_x, max_x, min_y, max_y)
+grid = draw_rock_paths(endpoints, min_x, max_x, min_y, max_y)
+print(grid)
--- day18.py
+++ day18.py
@@ -2,6 +2,7 @@
 
 import load_data as ld 
 import numpy as np 
+from collections import deque 
 
 def size_up(data):
     max_a = 0; max_b = 0; max_c = 0
@@ -69,10 +70,120 @@ max_a, max_b, max_c = size_up(data)
 grid = populate_grid(max_a, max_b, max_c)
 print(f'Example: {calculate_surface_area(grid, max_a, max_b, max_c)}') #64
 
-data = ld.load_data('input18.txt')  
-max_a, max_b, max_c = size_up(data)  
-grid = populate_grid(max_a, max_b, max_c)
-print(f'Part 1: {calculate_surface_area(grid, max_a, max_b, max_c)}') # 3564 was the answer
+# data = ld.load_data('input18.txt')  
+# max_a, max_b, max_c = size_up(data)  
+# grid = populate_grid(max_a, max_b, max_c)
+# print(f'Part 1: {calculate_surface_area(grid, max_a, max_b, max_c)}') # 3564 was the answer
 
 # Part 2
+def search_for_path_to_edge(grid, max_a, max_b, max_c, i, j, k):
+    # breadth-first search from day 12 
+    q = deque()
+    q.append((i, j, k))
+    visited = {(i, j, k)}
 
+    while(q):
+        x, y, z = q.popleft()
+        neighbors = [(x+1,y,z), (x-1,y,z), (x,y+1,z), (x,y-1,z), (x,y,z+1), (x,y,z-1)]
+        for xx, yy, zz in neighbors:
+            if((xx < 0) or (xx > max_a) or (yy < 0) or (yy > max_b) or (zz < 0) or (zz > max_c)): # checking if that coordinate exists in the grid
+                continue
+            if((xx, yy, zz) in visited): # checking if it's already been visited
+                continue
+            if(grid[xx][yy][zz] == 1): # checking if it's part of the lava and not open air
+                continue 
+            if((xx == 0) or (xx == max_a) or (yy == 0) or (yy == max_b) or (zz == 0) or (zz == max_c)): # checking if we've reached the edge
+                return True
+            visited.add((xx, yy, zz))
+            q.append((xx, yy, zz))
+    # if we exhaust the deque and have not returned True, then it is False (it is an interior pocket)
+    return False
+
+def recalculate_surface_area(grid, max_a, max_b, max_c):
+    surface_area = 0
+    for i in range(max_a + 1):
+        for j in range(max_b + 1):
+            for k in range(max_c + 1):
+                # pocket = True 
+                # if((i == 0) or (i == max_a)):
+                #     pocket = False
+                # else:
+                #     s = 0
+                #     for ii in range(i):
+                #         s += grid[ii][j][k]
+                #     if(s == 0):
+                #         pocket = False
+                #     else:
+                #         s = 0
+                #         for ii in range(max_a, i, -1):
+                #             s += grid[ii][j][k]
+                #         if(s == 0):
+                #             pocket = False
+
+                
+                # if((j == 0) or (j == max_b)):
+                #     pocket = False
+                # else:
+                #     s = 0
+                #     for jj in range(j):
+                #         s += grid[i][jj][k]
+                #     if(s == 0):
+                #         pocket = False
+                #     else:
+                #         s = 0
+                #         for jj in range(max_b, j, -1):
+                #             s += grid[i][jj][k]
+                #         if(s == 0):
+                #             pocket = False
+                
+                # if((k == 0) or (k == max_c)):
+                #     pocket = False
+                # else:
+                #     s = 0
+                #     for kk in range(k):
+                #         s += grid[i][j][kk]
+                #     if(s == 0):
+                #         pocket = False
+                #     else:
+                #         s = 0
+                #         for kk in range(max_c, k, -1):
+                #             s += grid[i][j][kk]
+                #         if(s == 0):
+                #             pocket = False
+                
+
+                # if((grid[i][j][k] == 1) and (pocket == False)):
+                
+                if(grid[i][j][k] == 1):
+                    exterior_surface = False 
+                    if((i == 0) or (i == max_a)):
+                        exterior_surface = True
+                    if((j == 0) or (j == max_b)):
+                        exterior_surface = True
+                    if((k == 0) or (k == max_c)):
+                        exterior_surface = True
+                    if(exterior_surface == False):
+                        exterior_surface = search_for_path_to_edge(grid, max_a, max_b, max_c, i, j, k)
+                    if(exterior_surface):
+                        surface_area += 6
+                        if(i > 0):
+                            if(grid[i-1][j][k] == 1):
+                                surface_area -= 1
+                        if(i < max_a):
+                            if(grid[i+1][j][k] == 1):
+                                surface_area -= 1
+                        if(j > 0):
+                            if(grid[i][j-1][k] == 1):
+                                surface_area -= 1
+                        if(j < max_b):
+                            if(grid[i][j+1][k] == 1):
+                                surface_area -= 1
+                        if(k > 0):
+                            if(grid[i][j][k-1] == 1):
+                                surface_area -= 1
+                        if(k < max_c):
+                            if(grid[i][j][k+1] == 1):
+                                surface_area -= 1
+    return surface_area
+
+print(f'Part 2: {recalculate_surface_area(grid, max_a, max_b, max_c)}') #  was the answer
